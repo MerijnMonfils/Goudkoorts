8fd7afdc100a64181aea91f5a865132254c91a00
DanishAfzal <d.afzal@hotmail.com>
2018-10-15 15:32:09 +0200

    Interfaces

--- MainView/MainView/Model/MoveAbles/MoveableObject.cs
+++ MainView/MainView/Model/MoveAbles/MoveableObject.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Goudkoorts.Model.MoveAbles
 {
-    class MoveableObject
+    interface  MoveableObject
     {
     }
 }
--- MainView/MainView/Model/Rails/Rail.cs
+++ MainView/MainView/Model/Rails/Rail.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Goudkoorts.Model.Rails
 {
-    class Rail
+    interface Rail
     {
     }
 }
